517c7da7174125747dca2f1f21bf5f9301a3ce65
GalMiles <58370322+GalMiles@users.noreply.github.com>
2019-12-14 01:14:49 +0200

    /

--- Menu.h
+++ Menu.h
@@ -21,9 +21,6 @@ public:
 	void showSellers() const;
  
 
-	//have to be moved to system class!!!
-	/*Seller **sellerArray(int size);*/
-
 private:
 	const char *options[OPTIONS_LENGTH] = { "Add a buyer", "Add a seller", "Add a product to seller's stock", "Add seller feedback", "Add a product to your shopping cart",
 											"Place an order", "Pay for an order", "Show all buyers", "Show all sellers", "Search for a product", "Exit" };
